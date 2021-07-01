cdd3e0ebe77fc61f76bd37c8de0ac8f30fa905c8
Mario David Riguera Castillo <mario.riguera@gmail.com>
2021-07-01 16:56:56 +0200

    Eventos de la pagina

---
+++ EventosPagina003/WebForm1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EventosPagina003
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            Response.Write("1) PreInit <br/>");
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            Response.Write("2) Init <br/>");
+        }
+        protected void Page_InitComplete(object sender, EventArgs e)
+        {
+            Response.Write("2) InitComplete <br/>");
+        }
+        protected void Page_PreLoad(object sender, EventArgs e)
+        {
+            Response.Write("2) PreLoad <br/>");
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("2) Load <br/>");
+        }
+    }
+}
