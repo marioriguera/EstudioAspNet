e2498531e45ecd859bf81925190f91c1549aaa22
Mario David Riguera Castillo <mario.riguera@gmail.com>
2021-07-02 17:42:20 +0200

    Calculos geometricos

---
+++ CalculosGeometricos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CalculosGeometricos
+{
+    class Program
+    {
+
+        static double CalculoCircunferencia(double radio)
+        {
+            return 2 * radio * Math.PI;
+        }
+        static double CalculoAreaCirculo(double radio)
+        {
+            return Math.PI * Math.Sqrt(radio);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculos geometricos \b");
+            Console.WriteLine("Introduzca el radio: \b");
+            double radio = Double.Parse(Console.ReadLine());
+            Console.WriteLine("La circunferencia tiene un valor de "+ CalculoCircunferencia(radio) +" cm y el valor del area es de "+ CalculoAreaCirculo(radio)+" cm cuadrados.");
+        }
+
+    }
+}
